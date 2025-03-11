5021c8d717eb2bbc1132565a0dd97c2df5438f24
HaseebsShaukat <haseebsShoukat30@gmail.com>
2025-03-11 13:54:06 +0500

    Writing Test Admin FUnctionality Added

--- controllers/testsController.js
+++ controllers/testsController.js
@@ -1,4 +1,4 @@
-const { getAllTests, createTest, getTestPartData, saveTestPartData, saveAnswerToDatabase} = require("../models/testsModel");
+const { getAllTests, createTest, getTestPartData,saveTestPartData, saveAnswerToDatabase} = require("../models/testsModel");
 
 
 // Controller to handle GET request to fetch all tests
@@ -31,7 +31,7 @@ exports.createTestController = async (req, res) => {
 
 
   
-// Controller to fetch test part data
+//Controller to fetch test part data
 exports.fetchTestPartData = async (req, res) => {
   const { testId, partName } = req.params;
   console.log("Request received: ", testId, partName);
@@ -55,59 +55,7 @@ exports.saveTestPart = async (req, res) => {
   }
 };
 
-// // Controller function to fetch test data
-// exports.getTestData = async (req, res) => {
-//   const { testId } = req.params;
-//   try {
-//     const testData = await fetchTestData(testId);
-//     if (!testData) {
-//       return res.status(404).json({ message: "Test not found" });
-//     }
-//     res.status(200).json(testData);
-//   } catch (error) {
-//     console.error("Error fetching test data:", error);
-//     res.status(500).json({ message: "Error fetching test data", error });
-//   }
-// };
 
-// // Controller function to save new test data
-// exports.createTestData = async (req, res) => {
-//   const testData = req.body;
-//   try {
-//     const savedTest = await saveTestData(testData);
-//     res.status(201).json(savedTest);
-//   } catch (error) {
-//     console.error("Error saving test data:", error);
-//     res.status(500).json({ message: "Error saving test data", error });
-//   }
-// };
-
-// // Controller function to update existing test data
-// exports.updateTestDataController = async (req, res) => {
-//   const { testId } = req.params;
-//   const testData = req.body;
-//   testData.testId = testId; // Ensure the testId is included for update
-//   try {
-//     const updatedTest = await updateTestData(testData);
-//     res.status(200).json(updatedTest);
-//   } catch (error) {
-//     console.error("Error updating test data:", error);
-//     res.status(500).json({ message: "Error updating test data", error });
-//   }
-// };
-
-  
-// Controller to fetch test part data
-exports.fetchTestPartData = async (req, res) => {
-  const { testId, partName } = req.params;
-  console.log("Request received: ", testId, partName);
-  try {
-    const data = await getTestPartData(testId, partName);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
 
 // Controller to save test part data
 exports.saveTestPart = async (req, res) => {
